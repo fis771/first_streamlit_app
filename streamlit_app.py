f1bb382e3f47700c037e3eeedb77c304e4c81977
fis771 <57124921+fis771@users.noreply.github.com>
2023-06-07 09:59:38 +0800

    Update streamlit_app.py
    adding text of 'kale, Spinach & Rocket smoothie'

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,4 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('kale, Spinach & Rocket smoothie')
